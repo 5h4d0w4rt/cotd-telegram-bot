4638804f66cb0bf4075bc5813dabdc5bfe54d0e9
Glazkov.A <glazkov.a@wildberries.ru>
2020-09-13 02:55:55 +0300

    Added ability to reply cringe to a specific message.

--- bot/handlers.py
+++ bot/handlers.py
@@ -1,7 +1,6 @@
 def unknown(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="nope")
 
-
 def start(update, context):
     pass
     # if update.effective_message.reply_to_message:
@@ -11,16 +10,18 @@ def start(update, context):
     # else:
     context.bot.send_message(chat_id=update.effective_chat.id, text="start")
 
-
 def cringe(update, context):
-    context.bot.send_sticker(chat_id=update.effective_chat.id,
-                             sticker=open('static/smileyOne.webp', 'rb'))
-
+    if hasattr(update.message.reply_to_message,'message_id'):
+        context.bot.send_sticker(chat_id=update.effective_chat.id,          
+                                reply_to_message_id=update.message.reply_to_message.message_id,
+                                sticker=open('static/smileyOne.webp', 'rb'))
+    else:
+        context.bot.send_sticker(chat_id=update.effective_chat.id,          
+                                sticker=open('static/smileyOne.webp', 'rb'))
 
 def oldfellow(update, context):
     context.bot.send_video(chat_id=update.effective_chat.id,
                            video=open('static/oldfellow.mp4', 'rb'))
-
 
 def kekw(update, context):
     context.bot.send_video(chat_id=update.effective_chat.id,
