53f6599fd9c021f271c76dd20c6edafc2bd20107
5h4d0w4rt <5h4d0w4rt@users.noreply.github.com>
2020-09-14 20:49:24 +0500

    added creating sticker pack prototype

--- cotd/handlers.py
+++ cotd/handlers.py
@@ -1,20 +1,27 @@
 import random
 import telegram
 import telegram.ext
-# TODO create class
-# that logs every handler call
-# so we do not need to implement logging for every handler out there
+import logging
 
 
-def unknown(update: telegram.Update, context: telegram.ext.CallbackContext) -> telegram.Message:
+def unknown(
+    update: telegram.Update,
+    context: telegram.ext.CallbackContext,
+) -> telegram.Message:
     return context.bot.send_message(chat_id=update.effective_chat.id, text="nope")
 
 
-def start(update: telegram.Update, context: telegram.ext.CallbackContext) -> telegram.Message:
+def start(
+    update: telegram.Update,
+    context: telegram.ext.CallbackContext,
+) -> telegram.Message:
     return context.bot.send_message(chat_id=update.effective_chat.id, text="start")
 
 
-def cringe(update: telegram.Update, context: telegram.ext.CallbackContext) -> telegram.Message:
+def cringe(
+    update: telegram.Update,
+    context: telegram.ext.CallbackContext,
+) -> telegram.Message:
     try:
         return context.bot.send_sticker(
             chat_id=update.effective_chat.id,
@@ -25,7 +32,10 @@ def cringe(update: telegram.Update, context: telegram.ext.CallbackContext) -> te
             chat_id=update.effective_chat.id, sticker=open('static/smileyOne.webp', 'rb'))
 
 
-def iscringe(update: telegram.Update, context: telegram.ext.CallbackContext) -> telegram.Message:
+def iscringe(
+    update: telegram.Update,
+    context: telegram.ext.CallbackContext,
+) -> telegram.Message:
     choices = ["based", "cringe"]
     final_choice = random.choice(choices)
     try:
@@ -44,7 +54,10 @@ def iscringe(update: telegram.Update, context: telegram.ext.CallbackContext) -> 
             chat_id=update.effective_chat.id, text='Can"t see cringe though')
 
 
-def oldfellow(update: telegram.Update, context: telegram.ext.CallbackContext) -> telegram.Message:
+def oldfellow(
+    update: telegram.Update,
+    context: telegram.ext.CallbackContext,
+) -> telegram.Message:
     try:
         return context.bot.send_video(
             chat_id=update.effective_chat.id,
@@ -55,7 +68,10 @@ def oldfellow(update: telegram.Update, context: telegram.ext.CallbackContext) ->
             chat_id=update.effective_chat.id, video=open('static/oldfellow.mp4', 'rb'))
 
 
-def kekw(update: telegram.Update, context: telegram.ext.CallbackContext) -> telegram.Message:
+def kekw(
+    update: telegram.Update,
+    context: telegram.ext.CallbackContext,
+) -> telegram.Message:
     try:
         return context.bot.send_video(
             chat_id=update.effective_chat.id,
@@ -66,5 +82,8 @@ def kekw(update: telegram.Update, context: telegram.ext.CallbackContext) -> tele
             chat_id=update.effective_chat.id, video=open('static/KEKW.mp4', 'rb'))
 
 
-def secret(update: telegram.Update, context: telegram.ext.CallbackContext) -> telegram.Message:
+def secret(
+    update: telegram.Update,
+    context: telegram.ext.CallbackContext,
+) -> telegram.Message:
     return context.bot.send_message(chat_id=update.effective_chat.id, text='bit.ly/2Ro39uJ')
--- cotd/prototypes/sticker-pack-creator.py
+++ cotd/prototypes/sticker-pack-creator.py
@@ -2,6 +2,8 @@
 import cotd.updater
 from cotd.updater import EnvConfig
 import os
+import telegram
+import telegram.ext
 
 
 def run(cotdbot: cotd.updater.COTDBot):
@@ -9,9 +11,25 @@ def run(cotdbot: cotd.updater.COTDBot):
     cotdbot.updater.idle()
 
 
+def start(
+    update: telegram.Update,
+    context: telegram.ext.CallbackContext,
+) -> telegram.Message:
+    return context.bot.send_message(chat_id=update.effective_chat.id, text="start")
+
+
 def main():
     config = cotd.updater.Config(env=EnvConfig(token=os.environ['COTD_TELEGRAM_BOT_TOKEN']))
     cotdbot = cotd.updater.COTDBot(config)
+    me = cotdbot.updater.bot.get_me()
+    print(me)
+    # print(cotdbot.updater.bot.get_sticker_set("VolumetricCringeStickerSet"))
+    cotdbot.updater.bot.create_new_sticker_set(
+        png_sticker=open("static/ezgif.com-resize.png", 'rb'),
+        name=f"VC_by_{me.username}",
+        title=f"VC_by_{me.username}",
+        user_id=int(me.id),
+        emojis="🙂")
     run(cotdbot)
 
 
