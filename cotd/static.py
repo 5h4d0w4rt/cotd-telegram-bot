3c80f3221f6af67a5618e8b46cbaa266c18fc5f8
5h4d0w4rt <5h4d0w4rt@users.noreply.github.com>
2020-09-18 23:02:32 +0500

    up static types

--- cotd/static.py
+++ cotd/static.py
@@ -5,21 +5,19 @@ from types import MappingProxyType
 
 @dataclass
 class Static:
-    kekw: typing.IO
-    oldfellow: typing.IO
-    sniff_dog: typing.IO
-    ribnikov: typing.IO
-    go_away: typing.IO
+    kekw: typing.BinaryIO
+    oldfellow: typing.BinaryIO
+    sniff_dog: typing.BinaryIO
+    ribnikov: typing.BinaryIO
+    go_away: typing.BinaryIO
     ozon_secret: str
 
 
-static = MappingProxyType({
+STATIC = Static(**MappingProxyType({
     'kekw': open('static/KEKW.mp4', 'rb'),
     'oldfellow': open("static/oldfellow.mp4", 'rb'),
     "ribnikov": open('static/ribnikov.based.mp4', 'rb'),
     'ozon_secret': 'bit.ly/2Ro39uJ',
     'sniff_dog': open("static/cringe-sniff-dog.jpg", 'rb'),
     "go_away": open('static/go_away.mp4', 'rb')
-})
-
-STATIC = Static(**static)
+}))
