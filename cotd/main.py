60e7814531d788c69b57829b57dc023572a9ec28
Glazkov.A <gearpanda@gmail.com>
2021-05-20 21:26:49 +0300

    fix fault.

--- cotd/main.py
+++ cotd/main.py
@@ -130,7 +130,7 @@ def main():
                 "group_index": 0,
                 "handlers": [
                     telegram.ext.MessageHandler(
-                        telegram.ext.Filters.BaseFilter(Filters.audio | Filters.voice),
+                        telegram.ext.Filters.audio,
                         functools.partial(voice_reaction),
                     ),
                     telegram.ext.MessageHandler(
