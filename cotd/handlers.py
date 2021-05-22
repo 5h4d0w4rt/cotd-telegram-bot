248e6dd1ca7a08316ade7306c3219f5daa7eab43
Glazkov.A <gearpanda@gmail.com>
2021-05-23 00:20:44 +0300

    потенциальный фикс voice_reaction

--- cotd/handlers.py
+++ cotd/handlers.py
@@ -91,22 +91,21 @@ def leftie_meme_detector(
 
 
 @logged_context
-@functools.partial(cacheable_handler, key="voice_spray", path="photo[0].file_id")
 def voice_reaction(
     update: telegram.Update,
     context: telegram.ext.CallbackContext,
     cache: typing.Type[MediaCache] = None,
     data: typing.Type[Static] = None,
-) -> typing.Union[telegram.Message, None]:
+) -> telegram.Message:
     roll_map = {1: True}
-    decision = roll_map.get(random.randint(0, 3))
+    decision = roll_map.get(random.randint(0, 2))
 
     if not decision:
         return None
 
     return context.bot.send_photo(
         chat_id=update.effective_chat.id,
-        reply_to_message_id=update.message.reply_to_message.message_id,
+        reply_to_message_id=update.effective_message.message_id,
         photo=cache.voice_spray or data.voice_spray,
     )
 
