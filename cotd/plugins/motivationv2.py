88838224ab629364fbee112a88c8ab4c5036adb1
5h4d0w4rt <5h4d0w4rt@gmail.com>
2021-10-03 11:39:17 +0500

    release new motivation

--- cotd/plugins/motivationv2.py
+++ cotd/plugins/motivationv2.py
@@ -5,62 +5,77 @@ import typing
 from PIL import Image, ImageFont, ImageDraw
 import io
 import uuid
+import re
 
 
 def motivation_handler_v2(
     update: telegram.Update,
     context: telegram.ext.CallbackContext,
 ) -> typing.Union[telegram.Message, None]:
+    results = []
     query = update.inline_query.query
     if query == "":
         return
 
-    results = [
-        telegram.InlineQueryResultArticle(
-            id=str(uuid.uuid4()),
-            title="pososi)",
-            input_message_content=telegram.InputTextMessageContent(query + " еблан"),
-        ),
-    ]
-    update.inline_query.answer(results)
+    if random.randint(0, 4) == 1:
+        results = [
+            telegram.InlineQueryResultArticle(
+                id=str(uuid.uuid4()),
+                title="👮‍♀️",
+                input_message_content=telegram.InputTextMessageContent("да забей, чел)"),
+            ),
+        ]
+        update.inline_query.answer(results)
+        return
 
-    # if random.randint(0, 4) == 1:
-    #     return context.bot.send_message(
-    #         chat_id=update.effective_chat.id,
-    #         text="да забей, чел",
-    #     )
-    # image = Image.open("static/motivator.jpg")
+    if re.search("мотивация\s\S+", query):
+        query = query.split("мотивация ")[1]
+        image = Image.open("static/motivator.jpg")
 
+        fontsize = 1  # starting font size
 
-#     fontsize = 1  # starting font size
+        # portion of image width you want text width to be
+        img_fraction = 0.50
 
-#     # portion of image width you want text width to be
-#     img_fraction = 0.50
+        font = ImageFont.truetype("static/lobster.ttf", fontsize)
+        while font.getsize(query)[0] < img_fraction * image.size[0]:
+            # iterate until the text size is just larger than the criteria
+            fontsize += 1
+            font = ImageFont.truetype("static/lobster.ttf", fontsize)
+        image_editable = ImageDraw.Draw(image)
+        W, H = image.size
+        w, h = image_editable.textsize(query, font)
 
-#     font = ImageFont.truetype("static/lobster.ttf", fontsize)
-#     while font.getsize(query)[0] < img_fraction * image.size[0]:
-#         # iterate until the text size is just larger than the criteria
-#         fontsize += 1
-#         font = ImageFont.truetype("static/lobster.ttf", fontsize)
+        width = (W - w) / 2
+        heigh = h / 5
+        # some color const
+        msg_color = "#FFFFFF"
+        shadow_color = "#121212"
+        # add shadow
+        image_editable.text((width - 2, heigh), query, font=font, fill=shadow_color)
+        image_editable.text((width + 2, heigh), query, font=font, fill=shadow_color)
+        image_editable.text((width, heigh - 2), query, font=font, fill=shadow_color)
+        image_editable.text((width, heigh + 2), query, font=font, fill=shadow_color)
+        # add text
+        image_editable.text((width, heigh), query, font=font, fill=msg_color)
+        # fake save
+        bio = io.BytesIO()
+        bio.name = "image.jpeg"
+        image.save(bio, "JPEG")
+        bio.seek(0)
 
-#     image_editable = ImageDraw.Draw(image)
-#     W, H = image.size
-#     w, h = image_editable.textsize(query, font)
+        msg = context.bot.send_photo(
+            chat_id=-517092184,
+            photo=bio,
+        )
+        photo_id = msg.photo[0].file_id
 
-#     width = (W - w) / 2
-#     heigh = h / 5
-#     # some color const
-#     msg_color = "#FFFFFF"
-#     shadow_color = "#121212"
-#     # add shadow
-#     image_editable.text((width - 2, heigh), query, font=font, fill=shadow_color)
-#     image_editable.text((width + 2, heigh), query, font=font, fill=shadow_color)
-#     image_editable.text((width, heigh - 2), query, font=font, fill=shadow_color)
-#     image_editable.text((width, heigh + 2), query, font=font, fill=shadow_color)
-#     # add text
-#     image_editable.text((width, heigh), query, font=font, fill=msg_color)
-#     # fake save
-#     bio = io.BytesIO()
-#     bio.name = "image.jpeg"
-#     image.save(bio, "JPEG")
-#     bio.seek(0)
+        results.append(
+            telegram.InlineQueryResultCachedPhoto(
+                id=str(uuid.uuid4()),
+                title="CachedPhoto",
+                photo_file_id=photo_id,
+            )
+        )
+
+        update.inline_query.answer(results)
