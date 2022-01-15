5ceeb8f9893966ab5a71b74191368c55dfceb9c0
Alex <gearpanda@gmail.com>
2022-01-15 15:18:39 +0300

    Update misc.py

--- cotd/plugins/misc.py
+++ cotd/plugins/misc.py
@@ -160,10 +160,6 @@ def patriot_reaction(
     update: telegram.Update,
     context: telegram.ext.CallbackContext,
 ) -> typing.Union[telegram.Message, None]:
-
-    if not _chance(0.5):
-        return None
-
     return context.bot.send_message(
         chat_id=update.effective_chat.id,
         reply_to_message_id=update.message.message_id,
