737b2a527848e6ba0f3b9a8100f68f1212d64336
5h4d0w4rt <5h4d0w4rt@gmail.com>
2022-04-09 07:42:35 +0500

    reformat for usability, enable on prod

--- cotd/plugins/cronjobs.py
+++ cotd/plugins/cronjobs.py
@@ -11,7 +11,7 @@ import cotd
 
 def you_made_it(context: telegram.ext.CallbackContext):
     context.bot.send_video_note(
-        chat_id=context.dispatcher._cotd_db, video_note=context.bot.static_content.doge_friday
+        chat_id=context.dispatcher._cotd_group, video_note=context.bot.static_content.doge_friday
     )
 
 # TODO remake in ctl keyboard
@@ -19,7 +19,7 @@ def cronjobsctl(update: telegram.Update, context: telegram.ext.CallbackContext):
     ctl = update.effective_message.text.removeprefix("/jobs").strip()
 
     jobs = "\n".join(
-        f"<code>{job.name}: \n {job.job} \n></code>" for job in context.dispatcher.job_queue.jobs()
+        f"<code>run:{job.name}</code>: \n {job.job} \n" for job in context.dispatcher.job_queue.jobs()
     )
 
     match ctl:
